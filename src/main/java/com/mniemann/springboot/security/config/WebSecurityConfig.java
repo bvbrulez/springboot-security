b0a97ec9098c915e21a8532e4ef74870fec64d7e
Markus Niemann <markus.niemann@lhind.dlh.de>
2021-07-20 14:15:06 +0200

    feat: add basic auth and whitelisting

--- src/main/java/com/mniemann/springboot/security/config/WebSecurityConfig.java
+++ src/main/java/com/mniemann/springboot/security/config/WebSecurityConfig.java
@@ -6,11 +6,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity // this is the place where we configure everything about security
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+
+    // whitelist for every single user should be able to get access to the
+    // - root
+    // - index
+    // - css
+    // - js -> permit all
+    // any request must be authenticated with the basic auth mechanism
+
     http.authorizeRequests()
         .antMatchers("/", "index", "/css/*", "/js/*")
         .permitAll()
